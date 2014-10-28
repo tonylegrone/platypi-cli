6d0272879350af35df0883c0ed1cb18321d1cecc
Donald Jones <donald@getplatypi.com>
2014-10-28 11:35:56 -0500

    Added command line msg helper.

---
+++ platypi-cli/helpers/msg.helper.ts
@@ -0,0 +1,15 @@
+﻿var colors = require('cmd-colors');
+
+export var error = (errorMsg: string) => {
+    var errorcolor = new colors().red,
+        msgcolor = new colors().magenta;
+
+    process.stdout.write(errorcolor('\r\nPlatypi-CLI Error: ') + msgcolor(errorMsg));
+};
+
+export var warning = (warningMsg: string) => {
+    var errorcolor = new colors().blue,
+        msgcolor = new colors().magenta;
+
+    process.stdout.write(errorcolor('\r\nPlatypi-CLI Warning: ') + msgcolor(warningMsg));
+};
