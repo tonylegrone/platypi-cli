f6fa0d88a8ab4fa963212fc799a079e09a82f833
Donald Jones <donald@getplatypi.com>
2014-10-28 17:05:20 -0500

    Added labels and logs to cmd msg helper.

--- platypi-cli/helpers/msg.helper.ts
+++ platypi-cli/helpers/msg.helper.ts
@@ -1,15 +1,27 @@
-﻿var colors = require('cmd-colors');
+﻿import util = require('util');
+
+var colors = require('cmd-colors');
+
+export var label = (labelMsg: string) => {
+    var labelColor = new colors().magentaBright;
+    process.stdout.write(labelColor(labelMsg) + '\n');
+};
+
+export var log = (logMsg: string) => {
+    var logColor = new colors().greenBright;
+    process.stdout.write(logColor(logMsg) + '\n');
+};
 
 export var error = (errorMsg: string) => {
     var errorcolor = new colors().red,
         msgcolor = new colors().magenta;
 
-    process.stdout.write(errorcolor('\r\nPlatypi-CLI Error: ') + msgcolor(errorMsg));
+    process.stdout.write(errorcolor('Platypi-CLI Error: ') + msgcolor(errorMsg) + '\n');
 };
 
 export var warning = (warningMsg: string) => {
     var errorcolor = new colors().blue,
         msgcolor = new colors().magenta;
 
-    process.stdout.write(errorcolor('\r\nPlatypi-CLI Warning: ') + msgcolor(warningMsg));
+    process.stdout.write(errorcolor('Platypi-CLI Warning: ') + msgcolor(warningMsg) + '\n');
 };
