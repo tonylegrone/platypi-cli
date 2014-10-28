9bb2feed6facea902ea804ebd6efc5e86b1f7eba
Donald Jones <donald@getplatypi.com>
2014-10-28 16:04:32 -0500

    Added directory utils.

---
+++ platypi-cli/utils/directory.utils.ts
@@ -0,0 +1,26 @@
+﻿import path = require('path');
+
+/*
+ * Returns the current working directory as an array of strings in order.
+ * i.e. /path/to/home/ = ['', 'path', 'to', 'home']
+ */
+export var dirToArray = (directory?: string): Array<string> => {
+    if (directory) {
+        return path.normalize(directory).split(path.sep);
+    }
+
+    return process.cwd().split(path.sep);
+};
+
+/*
+ * Returns the directory level above the current working directory as a string.
+ */
+export var upOneLevel = (directory?: string): string => {
+    var c: Array<string> = dirToArray(directory);
+
+    if (c.length > 1) {
+        c.slice(0, c.length - 1);
+    }
+
+    return c.join(path.sep);
+};
