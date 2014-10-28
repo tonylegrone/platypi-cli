1c71832b72f2742f21e02cee6ed0039474f20a69
Donald Jones <donald@getplatypi.com>
2014-10-28 17:09:41 -0500

    Added basic commands, wired lookup for platypi.json

--- platypi-cli/platypi.ts
+++ platypi-cli/platypi.ts
@@ -2,36 +2,49 @@
 
 import commander = require('commander');
 import msg = require('./helpers/msg.helper');
+import configfinder = require('./config/config.finder');
 
 var package = require('../package.json'),
+    prompt = require('prompt'),
     progressbar = require('simple-progress-bar'),
-    controlTypes = ['viewcontrol', 'injectable', 'repository', 'service', 'model', 'templatecontrol', 'attributecontrol'];
+    controlTypes = ['viewcontrol', 'injectable', 'repository', 'service', 'model', 'templatecontrol', 'attributecontrol'],
+    platypiConfig: config.IPlatypi = null;
+
+try {
+    platypiConfig = configfinder();
+} catch (e) {
+    
+}
 
 commander
     .version(package.version)
+    .usage('[command] [parameters..]')
     .command('create [type] [name]')
     .description('Create a new PlatypusTS project of type mobile or web. Default: mobile')
     .action((type, name) => {
-        if (!name) {
-            msg.warning('No type specified, using Mobile');
-            type = 'mobile';
-        }
-        if (!name) {
-            msg.warning('No name specified, using NewProject');
-            name = 'NewProject';
-        }
         process.exit(0);
     });
 
 commander
     .command('add <type> [name] [registered name]')
-    .description('Add a new control to your project. Types: [' + controlTypes.reduce((a, b) => { return a + ', ' + b }) + ']')
+    .description('Add a new control to your project. Types: [' + controlTypes.reduce((a, b) => { return a + ', ' + b; }) + ']')
     .action((type, name, registeredname) => {
         process.exit(0);
     });
 
 commander.parse(process.argv);
 
-// no command issued, display help
-commander.help();
+// command not matched
+if (commander.args.length > 0) {
+    msg.error('No command found matching ' + commander.args[0]);
+    process.exit(1);
+}
+
+// no command issued use the CLI q&a
+// commander.js TS definitions need to be updated using <any> for now.
+msg.label('Platypi Command Line Interface');
+msg.log('Version ' + package.version);
+msg.log('Now entering interactive project generation...');
+
+// use prompt to fill in platypi.json properties
 
