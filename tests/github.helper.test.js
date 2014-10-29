fbbe0c7cca4169702a0f58e3a54c57d078c1342d
Donald Jones <donald@getplatypi.com>
2014-10-29 15:35:41 -0500

    Updated github tests.

--- tests/github.helper.test.js
+++ tests/github.helper.test.js
@@ -1,39 +1,43 @@
 var should = require('should'),
     fs = require('fs'),
-    http = require('http'),
+    http = require('https'),
     package = require('../package.json'),
-    helper = require('../platypi-cli/helpers/github.helper');
+    helper = require('../platypi-cli/helpers/github.helper').helper;
 
 describe('github helper', function () {
     it('should be an object', function () {
         helper.should.be.an.Object;
     });
     
-    describe('getUrl', function () {
+    describe('_getUrl', function () {
         it('should be a function', function () {
-            helper.getUrl.should.be.a.Function;
+            helper._getUrl.should.be.a.Function;
         });
 
         it('should return a url using the current cli version', function () {
-            helper.getUrl(package.version).should.containEql(package.version);
+            helper._getUrl(package.version).should.containEql(package.version);
         });
 
         describe('the url', function () {
             it('should be valid & accessible for downloading templates', function () {
-                var url = helper.getUrl(package.version);
+                var url = helper._getUrl(package.version);
 
                 http.get(url, function (res) {
-                    res.statusCode.should.eql('200');
+                    res.statusCode.should.equal(302);
                 });
             });
         });
     });
 
     describe('downloadRepo', function () {
+        before(function (done) {
+            helper.downloadRepo().then(function () {
+                done();
+            });
+        });
+
         it('should create a cache dir', function () {
             fs.stat('../cache/', function (err, stats) {
-                (err === false).should.be.true;
-                (stats === true).should.be.true;
                 stats.should.be.an.Object;
                 stats.isDirectory().should.be.true;
             });
@@ -41,8 +45,6 @@ describe('github helper', function () {
 
         it('should download an archive of templates', function () {
             fs.stat('../cache/' + package.version + '.tar.gz', function (err, stats) {
-                (err === false).should.be.true;
-                (stats === true).should.be.true;
                 stats.should.be.an.Object;
                 stats.isFile().should.be.true;
             });            
