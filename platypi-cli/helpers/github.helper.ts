51130df78d8848b9bea565c53cebe8ccc1d8d04a
Donald Jones <donald@getplatypi.com>
2014-10-30 10:15:31 -0500

    Added github helper to match spec.

---
+++ platypi-cli/helpers/github.helper.ts
@@ -0,0 +1,47 @@
+﻿/// <reference path="../_references.d.ts" />
+
+import fs = require('fs');
+import http = require('https');
+import util = require('util');
+import promises = require('es6-promise');
+
+var package = require('../../package.json'),
+    baseUrl = 'https://github.com/Platypi/platypi-cli-templates/archive/',
+    Promise = promises.Promise;
+
+export class GithubHelper {
+    private _getUrl() {
+        return util.format('%s%s%s', baseUrl, package.version, '.tar.gz');
+    }
+
+    downloadRepo(path: string): Thenable<string> {
+        return new Promise((resolve, reject) => {
+            fs.mkdir(path + '/cache', (err) => {
+                var filepath = util.format('%s%s%s', path + '/cache/', package.version, '.tar.gz');
+                http.get(this._getUrl(), (res: any) => {
+                    var data = '';
+                    res.setEncoding('binary');
+
+                    res.on('data', (chunk) => {
+                        data += chunk;
+                    });
+
+                    res.on('error', (err) => {
+                        throw err;
+                    });
+
+                    res.on('end', () => {
+                        fs.writeFile(filepath, data, 'binary', (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                            resolve(filepath);
+                        });
+                    });
+                });
+            });
+        });
+    }
+}
+
+export var helper = new GithubHelper();
