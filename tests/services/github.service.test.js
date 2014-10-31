5346da31bc62e471fb92c408e33f35af78055bd7
Donald Jones <donald@getplatypi.com>
2014-10-31 18:23:14 -0500

    Linted.

--- tests/services/github.service.test.js
+++ tests/services/github.service.test.js
@@ -1,11 +1,12 @@
 var should = require('should') // jshint ignore:line
-    , service = require('../../platypi-cli/services/github/github.service')
+    , GithubService = require('../../platypi-cli/services/github/github.service')
     , path = require('path');
 
 describe('github service', function () {
     var returnedPath = '';
 
     before(function (done) {
+        var service = new GithubService();
         service.getRelease('0.0.1', path.normalize('.')).then(function (path) {
             returnedPath = path;
             done();
