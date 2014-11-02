daa82e06133e15ee87ccbbf31c489b66f7412899
Donald Jones <donald@getplatypi.com>
2014-11-02 12:49:07 -0600

    Github service now downloads the release in zip format.

--- platypi-cli/services/github/github.service.ts
+++ platypi-cli/services/github/github.service.ts
@@ -6,7 +6,7 @@ import util = require('util');
 class GithubService extends BaseService {
 
     getRelease(version: string, savePath: string, token?: string): Thenable<string> {
-        var host = 'https://codeload.github.com/Platypi/platypi-cli-templates/archive/'
+        var host = 'https://github.com/Platypi/platypi-cli-templates/archive/'
             , location = util.format('%s%s%s', host, version, '.zip');
 
         return this._downloadFile(location, savePath, token);
