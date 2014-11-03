6f4952fdbdced4b65e519d1e51734e83f3eab623
Donald Jones <donald@getplatypi.com>
2014-11-03 14:37:16 -0600

    Updated template.helper to match new spec.

--- platypi-cli/helpers/template.helper.ts
+++ platypi-cli/helpers/template.helper.ts
@@ -4,12 +4,18 @@ import fs = require('fs');
 import util = require('util');
 import path = require('path');
 import promises = require('es6-promise');
-import GithubService = require('../services/github/github.service');
 
 var package = require('../../package.json')
+    , admzip = require('adm-zip')
     , Promise = promises.Promise;
 
-export class TemplateHelper {
+class TemplateHelper<T extends IBaseService> {
+    service: T;
+    cacheDir = null;
+
+    constructor(Service: new () => T) {
+        this.service = new Service();
+    }
 
     private __makeCacheDir(appDataFolderPath: string): Thenable<string> {
         return new Promise((resolve, reject) => {
@@ -19,12 +25,14 @@ export class TemplateHelper {
                 if (err) {
                     if (err.code === 'EEXIST') {
                         // path already exists
+                        this.cacheDir = cacheFolder;
                         return resolve(cacheFolder);
                     } else {
                         return reject(err);
                     }
                 }
 
+                this.cacheDir = cacheFolder;
                 return resolve(cacheFolder);
             });
         });
@@ -48,17 +56,30 @@ export class TemplateHelper {
         });
     }
 
-    __downloadTemplates(appDataDir: string): Thenable<string> {
+    private __downloadTemplates(appDataDir: string): Thenable<string> {
         return this.__makeCacheDir(appDataDir)
             .then(this.__makeArchiveCacheDir)
             .then((archivePath) => {
                 var version = package.version || '0.0.1'
-                    , filePath = path.normalize(util.format('%s/%s.%s', archivePath, version, 'zip'))
-                    , service = new GithubService();
+                    , filePath = path.normalize(util.format('%s/%s.%s', archivePath, version, 'zip'));
 
-                return service.getRelease(version, filePath);
+                return this.service.getRelease(version, filePath);
+            });
+    }
+
+    updateTemplates(appDataDir: string): Thenable<string> {
+        return this.__downloadTemplates(appDataDir)
+            .then((zipLocation) => {
+                var extractDir = path.normalize(util.format('%s/%s', this.cacheDir, package.version));
+                var zip = admzip(zipLocation);
+
+                zip.extractAllTo(extractDir, true);
+
+                extractDir = path.normalize(util.format('%s/%s-%s', extractDir, 'platypi-cli-templates', package.version));
+
+                return extractDir;
             });
     }
 }
 
-export var helper = new TemplateHelper();
+export = TemplateHelper;
