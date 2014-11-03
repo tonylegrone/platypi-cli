29592d49f0ee917bac2e738363ace911b875d189
Donald Jones <donald@getplatypi.com>
2014-11-03 12:04:39 -0600

    Updated github service spec.

--- tests/services/github.service.test.js
+++ tests/services/github.service.test.js
@@ -1,9 +1,34 @@
 var should = require('should') // jshint ignore:line
     , GithubService = require('../../platypi-cli/services/github/github.service')
     , path = require('path')
-    , fs = require('fs');
+    , fs = require('fs')
+    , http = require('https')
+    , package = require('../../package.json');
 
 describe('github service', function () {
+
+    describe('private __getUrl method', function () {
+        var service = new GithubService();
+
+        it('should be a function', function () {
+            service.__getUrl.should.be.a.Function;
+        });
+
+        it('should return a url using the current cli version', function () {
+            service.__getUrl(package.version).should.containEql(package.version);
+        });
+
+        describe('the url', function () {
+            it('should be valid & accessible for downloading templates', function () {
+                var url = service.__getUrl(package.version);
+
+                http.get(url, function (res) {
+                    res.statusCode.should.equal(302 || 200);
+                });
+            });
+        });
+    });
+
     var returnedPath = '';
 
     before(function (done) {
