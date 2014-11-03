c3d9a35344d38fcb5023a03d266a13a10502ca2d
Donald Jones <donald@getplatypi.com>
2014-11-03 12:04:51 -0600

    Updated github.service to match spec.

--- platypi-cli/services/github/github.service.ts
+++ platypi-cli/services/github/github.service.ts
@@ -5,9 +5,14 @@ import util = require('util');
 
 class GithubService extends BaseService {
 
+    baseUrl = 'https://github.com/Platypi/platypi-cli-templates/archive/';
+
+    private __getUrl(version: string) {
+        return util.format('%s/%s.zip', this.baseUrl, version);
+    }
+
     getRelease(version: string, savePath: string, token?: string): Thenable<string> {
-        var host = 'https://github.com/Platypi/platypi-cli-templates/archive/'
-            , location = util.format('%s%s%s', host, version, '.zip');
+        var location = this.__getUrl(version);
 
         return this._downloadFile(location, savePath, token);
     }
