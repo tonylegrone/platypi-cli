5cf4a17c792f25bc3bc4cb1e7df384191fd9b2a8
Donald Jones <donald@getplatypi.com>
2014-11-03 16:25:02 -0600

    Added cli config interface

--- platypi-cli/_references.d.ts
+++ platypi-cli/_references.d.ts
@@ -1,4 +1,5 @@
 ﻿/// <reference path="typings/tsd.d.ts" />
 /// <reference path="config/project/iconfig.interface.ts" />
 /// <reference path="services/ibase.service.ts" />
+/// <reference path="config/cli/iplatypicli.config.ts" />
  
---
+++ platypi-cli/config/cli/iplatypicli.config.ts
@@ -0,0 +1,27 @@
+﻿module config {
+    interface IPlatypiCliConfig {
+        templates: {
+            lastUpdated: Date;
+            projects: {
+                mobile: string;
+                web: string;
+            };
+            controls: {
+                base: {
+                    attribute: string;
+                    injectable: string;
+                    model: string;
+                    repository: string;
+                    service: string;
+                    template: string;
+                };
+                mobile: {
+                    viewcontrol: string;
+                };
+                web: {
+                    viewcontrol: string;
+                };
+            }
+        };
+    }
+}
