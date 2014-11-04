ed13476609d69b4c04e120361ca0b618df91e041
Donald Jones <donald@getplatypi.com>
2014-11-04 09:06:01 -0600

    Added Spec for platypi-cli config.

---
+++ tests/config/cli/platypicli.config.test.js
@@ -0,0 +1,45 @@
+var should = require('should') // jshint ignore:line
+    , CliConfig = require('../../../platypi-cli/config/cli/platypicli.config');
+
+describe('Platypi-CLI Config', function () {
+    var config = CliConfig.config;
+    it('should exist and be an object', function () {
+        should.exist(config);
+        config.should.be.an.Object;
+    });
+
+    describe('updateConfig', function () {
+        var error = null;
+
+        before(function (done) {
+            config.updateConfig().then(null,function(err) {
+                error = err;
+                done();
+            });
+        });
+
+        it('should fail, no config loaded', function () {
+            should.exist(error);
+            error.should.equal('No config loaded!');
+        });
+    });
+
+    describe('loadConfig', function () {
+        var output = null;
+
+        before(function (done) {
+            config.loadConfig().then(function (configOut) {
+                output = configOut;
+                done();
+            }, function (err) {
+                throw err;
+                done();
+            });
+        });
+
+        it('should load the config file if it exists', function () {
+            should.exist(output);
+            output.should.be.an.Object;
+        });
+    });
+});
