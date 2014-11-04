0d0cfdc1bc0134f9643c004c2309ce1b8fe1060e
Donald Jones <donald@getplatypi.com>
2014-11-04 09:06:26 -0600

    Updated config handler to match spec.

--- platypi-cli/config/cli/platypicli.config.ts
+++ platypi-cli/config/cli/platypicli.config.ts
@@ -2,39 +2,59 @@
 import path = require('path');
 import util = require('util');
 import dirutil = require('../../utils/directory.utils');
+import promises = require('es6-promise');
+
+var Promise = promises.Promise;
 
 export class PlatypiCliConfig {
     config: config.IPlatypiCliConfig = null;
     configPath: string = '';
 
-    loadConfig(): Thenable<any> {
-        return dirutil.appDataDir().then((appDataDir) => {
-            var configPath = path.normalize(util.format('%s/%s', appDataDir, 'cli.json'));
-            fs.readFile(configPath, 'utf8', (err, data) => {
-                if (err) {
-                    throw err;
-                }
+    setConfig(newConfig: config.IPlatypiCliConfig) {
+        this.config = newConfig;
+    }
 
-                this.configPath = configPath;
-                this.config = JSON.parse(data);
+    getConfig(): Thenable<config.IPlatypiCliConfig> {
+        if (config) {
+            return Promise.resolve(this.config);
+        } else {
+            return this.loadConfig();
+        }
+    }
 
-                return this.config;
+    loadConfig(): Thenable<config.IPlatypiCliConfig> {
+        return new Promise((resolve, reject) => {
+            dirutil.appDataDir().then((appDataDir) => {
+                var configPath = path.normalize(util.format('%s/%s', appDataDir, 'cli.json'));
+                fs.readFile(configPath, 'utf8', (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    this.configPath = configPath;
+                    this.config = JSON.parse(data);
+
+                    resolve(this.config);
+                });
             });
         });
     }
 
-    updateConfig(): Thenable<any> {
-        return dirutil.appDataDir().then((appDataDir) => {
-            var configPath = path.normalize(util.format('%s/%s', appDataDir, 'cli.json'));
-            if (!this.config) {
-                throw 'No config loaded!';
-            }
-            fs.writeFile(configPath, JSON.stringify(this.config), (err) => {
-                if (err) {
-                    throw err;
+    updateConfig(): Thenable<config.IPlatypiCliConfig> {
+        return new Promise((resolve, reject) => {
+            dirutil.appDataDir().then((appDataDir) => {
+                var configPath = path.normalize(util.format('%s/%s', appDataDir, 'cli.json'));
+                if (!this.config) {
+                    return reject('No config loaded!');
                 }
-                this.configPath = configPath;
-                return this.config;
+                fs.writeFile(configPath, JSON.stringify(this.config), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    this.configPath = configPath;
+
+                    resolve(this.config);
+                });
             });
         });
     }
