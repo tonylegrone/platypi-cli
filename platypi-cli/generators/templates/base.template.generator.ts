e4f7950798b703b9631d83b777c67d5bc4a94d01
Donald Jones <donald@getplatypi.com>
2014-11-03 18:20:32 -0600

    Added base template generator for template files.

---
+++ platypi-cli/generators/templates/base.template.generator.ts
@@ -0,0 +1,16 @@
+﻿import TemplateHelper = require('../../helpers/template.helper');
+import GithubService = require('../../services/github/github.service');
+import CliConfig = require('../../config/cli/platypicli.config');
+
+class BaseTemplateGenerator {
+    helper = null;
+    config = null;
+
+    constructor() {
+        this.helper = new TemplateHelper(GithubService);
+        this.config = CliConfig.config;
+    }
+
+}
+
+export = BaseTemplateGenerator;
