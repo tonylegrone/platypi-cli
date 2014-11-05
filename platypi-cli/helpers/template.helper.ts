d7ff6e60d021896b221a43c0c16ead73ad872d68
Donald Jones <donald@getplatypi.com>
2014-11-05 15:03:33 -0600

    Further refactoring, removed unnecessary promise references.

--- platypi-cli/helpers/template.helper.ts
+++ platypi-cli/helpers/template.helper.ts
@@ -2,13 +2,11 @@
 
 import util = require('util');
 import path = require('path');
-import promises = require('es6-promise');
 import PlatypiCliConfig = require('../config/cli/platypicli.config');
 import fileutils = require('../utils/file.utils');
 
 var package = require('../../package.json')
     , admzip = require('adm-zip')
-    , Promise = promises.Promise
     , cliConfig = PlatypiCliConfig.config;
 
 class TemplateHelper<T extends IBaseService> {
@@ -20,37 +18,33 @@ class TemplateHelper<T extends IBaseService> {
     }
 
     private __makeCacheDir(appDataFolderPath: string): Thenable<string> {
-        return new Promise((resolve, reject) => {
-            var cacheFolder = path.normalize(util.format('%s/%s', appDataFolderPath, 'cache'));
+        var cacheFolder = path.normalize(util.format('%s/%s', appDataFolderPath, 'cache'));
 
-            fileutils.mkdir(cacheFolder).then(() => {
+        return fileutils.mkdir(cacheFolder).then(() => {
+            this.cacheDir = cacheFolder;
+            return cacheFolder;
+        }, (err) => {
+            if (err.code === 'EEXIST') {
+                // path already exists
                 this.cacheDir = cacheFolder;
-                return resolve(cacheFolder);
-            }, (err) => {
-                if (err.code === 'EEXIST') {
-                    // path already exists
-                    this.cacheDir = cacheFolder;
-                    return resolve(cacheFolder);
-                } else {
-                    return reject(err);
-                }
-            });
+                return cacheFolder;
+            } else {
+                return err;
+            }
         });
     }
 
     private __makeArchiveCacheDir(cacheFolderPath: string): Thenable<string> {
-        return new Promise((resolve, reject) => {
-            var archiveFolder = path.normalize(util.format('%s/%s/', cacheFolderPath, 'archives'));
-            fileutils.mkdir(archiveFolder).then(() => {
-                return resolve(archiveFolder);
-            }, (err) => {
-                if (err.code === 'EEXIST') {
-                    // path already exists
-                    return resolve(archiveFolder);
-                } else {
-                    return reject(err);
-                }
-            });
+        var archiveFolder = path.normalize(util.format('%s/%s/', cacheFolderPath, 'archives'));
+        return fileutils.mkdir(archiveFolder).then(() => {
+            return archiveFolder;
+        }, (err) => {
+            if (err.code === 'EEXIST') {
+                // path already exists
+                return archiveFolder;
+            } else {
+                return err;
+            }
         });
     }
 
