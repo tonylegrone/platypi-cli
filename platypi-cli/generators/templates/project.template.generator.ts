38f5dc67da25c4c9d9041bee71bf52f39933c019
Donald Jones <donald@getplatypi.com>
2014-11-05 11:50:17 -0600

    Updated for changes to base class.

--- platypi-cli/generators/templates/project.template.generator.ts
+++ platypi-cli/generators/templates/project.template.generator.ts
@@ -1,1 +1,13 @@
-﻿ 
+﻿/// <reference path="../../_references.d.ts" />
+
+import BaseTemplateGenerator = require('./base.template.generator');
+
+class ProjectTemplateGenerator extends BaseTemplateGenerator {
+    constructor(type: string, environmentVariables: Array<config.IEnvironmentVariable>) {
+        super('project', type, environmentVariables);
+    }
+
+
+}
+
+export = ProjectTemplateGenerator;
