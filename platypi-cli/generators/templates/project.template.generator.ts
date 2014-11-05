5e59ee5ff6b9d9178720db15af825d169b0bf08e
Donald Jones <donald@getplatypi.com>
2014-11-05 17:09:54 -0600

    Added generateProject method.

--- platypi-cli/generators/templates/project.template.generator.ts
+++ platypi-cli/generators/templates/project.template.generator.ts
@@ -1,12 +1,30 @@
 ﻿/// <reference path="../../_references.d.ts" />
 
 import BaseTemplateGenerator = require('./base.template.generator');
+import fileutils = require('../../utils/file.utils');
+import path = require('path');
 
 class ProjectTemplateGenerator extends BaseTemplateGenerator {
     constructor(type: string, environmentVariables: Array<config.IEnvironmentVariable>) {
         super('project', type, environmentVariables);
     }
 
+    generateProject(projectConfig: config.IPlatypi, configPath?: string): Thenable<string> {
+        console.log('Extracting templates to: ' + process.cwd());
+        return this._copyTemplateTo(process.cwd()).then((folder) => {
+            var publicPath = path.join(folder, 'public');
+
+            if (!configPath || configPath === '') {
+                configPath = path.join(folder, 'platypi.json');
+            }
+
+            projectConfig.public = publicPath;
+
+            return fileutils.writeFile(configPath, JSON.stringify(projectConfig)).then(() => {
+                return folder;
+            });
+        });
+    }
 
 }
 
