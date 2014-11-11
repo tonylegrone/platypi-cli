8de780ff5189dfdda2ac6df9d02ffd638426a191
Donald Jones <donald@getplatypi.com>
2014-11-11 17:49:49 -0600

    Wired up the 'create project' command

--- platypi-cli/platypi.ts
+++ platypi-cli/platypi.ts
@@ -5,6 +5,7 @@ import msg = require('./helpers/msg.helper');
 import ConfigFinder = require('./config/project/config.finder');
 import ConfigGenerator = require('./generators/platypiconfig.generator');
 import ProjectGenerator = require('./generators/templates/project.template.generator');
+import PlatypiConfig = require('./config/project/platypi.config');
 
 var package = require('../package.json')
 // TODO:   , progressbar = require('simple-progress-bar')
@@ -16,6 +17,24 @@ var identifyApplication = () => {
     msg.log('Version ' + package.version);
 };
 
+var parseVariables = (newConfig: config.IPlatypi): Array<config.IEnvironmentVariable> => {
+    var environmentVariables: Array<config.IEnvironmentVariable> = []
+        , configKeys = Object.keys(newConfig);
+
+    configKeys.forEach((key) => {
+        var value = newConfig[key];
+        if (!(value instanceof Array)) {
+            var envVar: config.IEnvironmentVariable = {
+                name: key,
+                value: value
+            };
+            environmentVariables.push(envVar);
+        }
+    });
+
+    return environmentVariables;
+};
+
 identifyApplication();
 
 commander
@@ -24,7 +43,20 @@ commander
     .command('create [type] [name]')
     .description('Create a new PlatypusTS project of type mobile or web. Default: web')
     .action((type, name) => {
-        process.exit(0);
+        var newConfig = (type === 'mobile' ? PlatypiConfig.CreateNewMobileConfig() : PlatypiConfig.CreateNewWebConfig());
+
+        newConfig.name = name;
+
+        var environmentVariables = parseVariables(newConfig);
+
+        var projectGen = new ProjectGenerator(newConfig.type, environmentVariables);
+        projectGen.generateProject(newConfig).then((path) => {
+            msg.log('New Project at: ' + path);
+            process.exit(0);
+        }, (err) => {
+            msg.error(err);
+            process.exit(1);
+        });
     });
 
 commander
@@ -49,19 +81,7 @@ commander
         // generate project from command prompts
         msg.log('Now entering interactive project generation...');
         ConfigGenerator().then((newConfig) => {
-            var environmentVariables: Array<config.IEnvironmentVariable> = []
-                , configKeys = Object.keys(newConfig);
-
-            configKeys.forEach((key) => {
-                var value = newConfig[key];
-                if (!(value instanceof Array)) {
-                    var envVar: config.IEnvironmentVariable = {
-                        name: key,
-                        value: value
-                    };
-                    environmentVariables.push(envVar);
-                }
-            });
+            var environmentVariables = parseVariables(newConfig);
 
             var projectGen = new ProjectGenerator(newConfig.type, environmentVariables);
             return projectGen.generateProject(newConfig);
