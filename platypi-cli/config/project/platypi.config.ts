794c0ed2d53ed3a9fbe17ed8a4aec0fdbd857a8d
Donald Jones <donald@getplatypi.com>
2014-11-11 11:52:38 -0600

    Added base config class for default values and getters/setters

---
+++ platypi-cli/config/project/platypi.config.ts
@@ -0,0 +1,32 @@
+﻿class Config implements config.IPlatypi {
+    private __name: string = 'New Platypi Project';
+    private __author = 'Platypi';
+    private __homepage = 'http://getplatypi.com';
+    private __type = 'web';
+
+    get name(): string {
+        return this.__name;
+    }
+
+    set name(value: string) {
+        this.__name = value;
+    }
+
+    get author(): string {
+        return this.__author;
+    }
+
+    set author(value: string) {
+        this.__author = value;
+    }
+
+    get type(): string {
+        return this.__type;
+    }
+
+    set type(value: string) {
+        this.__type = value.toLowerCase();
+    }
+
+
+}
