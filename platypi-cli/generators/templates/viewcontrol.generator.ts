1945f27d32f7c080451cb292c14c3c16e878b267
Donald Jones <donald@getplatypi.com>
2014-11-14 17:11:23 -0600

    Added env var values.

--- platypi-cli/generators/templates/viewcontrol.generator.ts
+++ platypi-cli/generators/templates/viewcontrol.generator.ts
@@ -2,12 +2,12 @@
 
 import BaseTemplateGenerator = require('./base.template.generator');
 import path = require('path');
-import promises = require('es6-promise');
-
-var Promise = promises.Promise;
 
 class ViewControlTemplateGenerator extends BaseTemplateGenerator {
-    constructor(type: string, environmentVariables: Array<config.IEnvironmentVariable>) {
+    constructor(name: string, type: string, registeredName?: string) {
+        var environmentVariables: Array<config.IEnvironmentVariable> = [];
+        environmentVariables.push(<config.IEnvironmentVariable>{ name: 'name', value: name });
+        environmentVariables.push(<config.IEnvironmentVariable>{ name: 'registerName', value: registeredName || name });
         super('viewcontrol', type, environmentVariables);
     }
 
@@ -15,7 +15,11 @@ class ViewControlTemplateGenerator extends BaseTemplateGenerator {
         console.log('Creating ViewControl...');
         var viewControlPath = path.join(projectConfig.public, 'viewcontrols');
         return this._copyTemplateTo(viewControlPath).then((newPath) => {
-            projectConfig.addViewControl(this.instanceName, 'viewcontrol', this.registeredName);
+            var vcType = 'viewcontrol';
+            if (projectConfig.type === 'web') {
+                vcType = 'webviewcontrol';
+            }
+            projectConfig.addViewControl(this.instanceName, vcType, this.registeredName);
             return newPath;
         });
     }
