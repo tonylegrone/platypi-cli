965cee5d5f141705970d3ee20b5b5cfde30f2679
Donald Jones <donald@getplatypi.com>
2014-11-18 14:07:05 -0600

    Added injectable template generator.

---
+++ platypi-cli/generators/templates/injectable.template.generator.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../_references.d.ts" />
+
+import BaseTemplateGenerator = require('./base.template.generator');
+import path = require('path');
+
+class InjectableTemplateGenerator extends BaseTemplateGenerator {
+    constructor(name: string, registeredName?: string) {
+        var environmentVariables: Array<config.IEnvironmentVariable> = [
+            {
+                name: 'name',
+                value: name
+            },
+            {
+                name: 'registername',
+                value: (registeredName && registeredName !== '' ? registeredName : name)
+            }];
+        super('injectable', 'base', environmentVariables);
+    }
+
+    generate(projectConfig: config.IPlatypi): Thenable<string> {
+        console.log('Creating Injectable...');
+        var injectablePath = path.join(projectConfig.public, 'common/injectables');
+        return this._copyTemplateTo(injectablePath).then((newPath) => {
+            projectConfig.addInjectable(this.instanceName, 'injectable', this.registeredName);
+            return projectConfig.save().then(() => {
+                return newPath;
+            });
+        });
+    }
+}
+
+export = InjectableTemplateGenerator;
