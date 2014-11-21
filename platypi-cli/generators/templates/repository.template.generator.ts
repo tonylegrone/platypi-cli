3f53de641c1e0607e111497d2f6801b506391a05
Donald Jones <donald@getplatypi.com>
2014-11-20 18:59:28 -0600

    Added repository generator.

---
+++ platypi-cli/generators/templates/repository.template.generator.ts
@@ -0,0 +1,34 @@
+/// <reference path="../../_references.d.ts" />
+
+import BaseTemplateGenerator = require('./base.template.generator');
+import path = require('path');
+
+class RepositoryTemplateGenerator extends BaseTemplateGenerator {
+    constructor(name: string, registeredName: string) {
+        var environmentVariables: Array<config.IEnvironmentVariable> = [
+            {
+                name: 'name',
+                value: name
+            },
+            {
+                name: 'registername',
+                value: (registeredName && registeredName !== '' ? registeredName : name)
+            }];
+        super('repository', 'base', environmentVariables);
+    }
+
+
+    generate(projectConfig: config.IPlatypi): Thenable<string> {
+        console.log('Creating Repository...');
+        var repositoryPath = path.join(projectConfig.public, 'common/injectables');
+        return this._copyTemplateTo(repositoryPath).then((newPath) => {
+            projectConfig.addRepository(this.instanceName, 'repository', this.registeredName);
+            return projectConfig.save().then(() => {
+                return newPath;
+            });
+        });
+    }
+}
+
+export = RepositoryTemplateGenerator;
+
