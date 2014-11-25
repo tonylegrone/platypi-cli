41946dbc6bc6e444472a469f302c374459e1ecce
Donald Jones <donald@getplatypi.com>
2014-11-25 12:04:40 -0600

    Fixed config structure.

--- platypi-cli/config/cli/iplatypicli.config.ts
+++ platypi-cli/config/cli/iplatypicli.config.ts
@@ -23,17 +23,17 @@
                 web: {
                     viewcontrol: string;
                 };
-            }
-        };
-        projectStruct: Array<string>;
-        controlLocation: {
-            attribute: string;
-            injectable: string;
-            model: string;
-            repository: string;
-            service: string;
-            templatecontrol: string;
-            viewcontrol: string;
+            };
+            projectStruct: Array<string>;
+            controlLocation: {
+                attribute: string;
+                injectable: string;
+                model: string;
+                repository: string;
+                service: string;
+                templatecontrol: string;
+                viewcontrol: string;
+            };
         };
    }
 }
