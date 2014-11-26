e17a20fb00fac62c98acc27ba53f1c5c8831a53b
Donald Jones <donald@getplatypi.com>
2014-11-26 17:02:16 -0600

    Moved a few functions and console logging.

--- platypi-cli/globals.ts
+++ platypi-cli/globals.ts
@@ -1,2 +1,13 @@
 /// <reference path="_references.d.ts" />
+
+import msg = require('./helpers/msg.helper');
+
+export var console = msg;
+
 export var package = require('../package.json');
+
+export var identifyApplication = () => {
+    msg.label('Platypi Command Line Interface');
+    msg.log('Version ' + package.version);
+};
+
