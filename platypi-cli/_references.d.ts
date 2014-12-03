3e609964dc82ec5ac2b50fa509f2ad484f583697
Donald Jones <donald@getplatypi.com>
2014-12-03 14:20:23 -0600

    Refactored interfaces to .d.ts instead of .ts to avoid compiling.

--- platypi-cli/_references.d.ts
+++ platypi-cli/_references.d.ts
@@ -1,8 +1,8 @@
 ﻿/// <reference path="typings/tsd.d.ts" />
-/// <reference path="config/project/iconfig.interface.ts" />
-/// <reference path="services/ibase.service.ts" />
-/// <reference path="config/cli/iplatypicli.config.ts" />
-/// <reference path="generators/templates/ienvironmentvariable.interface.ts" />
-/// <reference path="providers/itemplateprovider.interface.ts" />
+/// <reference path="config/project/iconfig.interface.d.ts" />
+/// <reference path="services/ibase.service.d.ts" />
+/// <reference path="config/cli/iplatypicli.config.d.ts" />
+/// <reference path="generators/templates/ienvironmentvariable.interface.d.ts" />
+/// <reference path="providers/itemplateprovider.interface.d.ts" />
 
-/// <reference path="generators/templates/igenerator.interface.ts" />
+/// <reference path="generators/templates/igenerator.interface.d.ts" />
