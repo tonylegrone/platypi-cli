4e2e15bee4b9303863265f3e5cbff2a13d026eee
Donald Jones <donald@getplatypi.com>
2014-12-05 16:45:24 -0600

    CLI-10 #time 5m Added comments

--- platypi-cli/handlers/references.handler.ts
+++ platypi-cli/handlers/references.handler.ts
@@ -1,9 +1,16 @@
 /// <reference path="../_references.d.ts" />
 import fileutils = require('../utils/file.utils');
-
+/**
+ *  Contains methods for handling a project's _references.d.ts file.
+ */
 class ReferencesHandler {
     constructor(private location: string  = './public/_references.d.ts') {}
 
+    /**
+     *  Add a reference to the current project's _references.d.ts file.
+     *  @param referencePath The relative path to the interface to be referenced.
+     *  @param referenceType The control type of the interface.
+     */
     addReference(referencePath: string, referenceType?: string): Thenable<any> {
         return fileutils.readFile(this.location).then((referenceData: string) => {
             var typePos = -1;
@@ -21,6 +28,10 @@ class ReferencesHandler {
         });
     }
 
+    /**
+     *  Creates a new TypeScript reference string from the provided path.
+     *  @param referencePath The relative path to the interface to be referenced.
+     */
     private __newReferenceString(referencePath: string): String {
         return '/// <reference path=\"' + referencePath + '\" />';
     }
