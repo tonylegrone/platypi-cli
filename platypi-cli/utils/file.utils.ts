6e3586fbd322ea6f9b67deff92ca6844aa4483df
Donald Jones <donald@getplatypi.com>
2014-12-05 16:26:09 -0600

    CLI-10 #time 30m Added appendFileAt function for adding text at an index of a file.

--- platypi-cli/utils/file.utils.ts
+++ platypi-cli/utils/file.utils.ts
@@ -27,6 +27,13 @@ export var appendFIle = (location: string, data: string): Thenable<any> => {
     });
 };
 
+export var appendFileAt = (location: string, index: number, toAppend: string): Thenable<any> => {
+    return readFile(location, { encoding = 'utf8' }).then((data: string) => {
+        var newData = data.substring(0, index) + toAppend + data.substring(index, data.length);
+        return writeFile(location, newData);
+    });
+};
+
 export var stat = (location: string): Thenable<fs.Stats> => {
     return asyncCall((resolver) => {
         fs.stat(location, resolver);
