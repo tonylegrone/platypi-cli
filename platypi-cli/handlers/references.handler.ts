73c3e6d609debdf2529b2e0a1b24a63bd5b06660
Donald Jones <donald@getplatypi.com>
2014-12-08 14:51:05 -0600

    CLI-10 #time 1h Refactored references handler.

--- platypi-cli/handlers/references.handler.ts
+++ platypi-cli/handlers/references.handler.ts
@@ -1,18 +1,18 @@
 /// <reference path="../_references.d.ts" />
+
 import fileutils = require('../utils/file.utils');
+
 /**
  *  Contains methods for handling a project's _references.d.ts file.
  */
 class ReferencesHandler {
-    constructor(private location: string  = './public/_references.d.ts') {}
-
     /**
      *  Add a reference to the current project's _references.d.ts file.
      *  @param referencePath The relative path to the interface to be referenced.
      *  @param referenceType The control type of the interface.
      */
-    addReference(referencePath: string, referenceType?: string): Thenable<any> {
-        return fileutils.readFile(this.location).then((referenceData: string) => {
+    static addReference(referenceFileLocation: string, referencePath: string, referenceType?: string): Thenable<any> {
+        return fileutils.readFile(referenceFileLocation).then((referenceData: string) => {
             var typePos = -1;
 
             if (referenceType) {
@@ -21,9 +21,9 @@ class ReferencesHandler {
                 typePos = referenceData.indexOf(referenceTypeComment);
                 typePos = typePos + referenceTypeComment.length;
 
-                return fileutils.appendFileAt(this.location, typePos, '\n' + this.__newReferenceString(referencePath));
+                return fileutils.appendFileAt(referenceFileLocation, typePos, '\n' + this.newReferenceString(referencePath));
             } else {
-                return fileutils.appendFIle(this.location, '\n' + this.__newReferenceString(referencePath));
+                return fileutils.appendFIle(referenceFileLocation, '\n' + this.newReferenceString(referencePath));
             }
         });
     }
@@ -32,7 +32,7 @@ class ReferencesHandler {
      *  Creates a new TypeScript reference string from the provided path.
      *  @param referencePath The relative path to the interface to be referenced.
      */
-    private __newReferenceString(referencePath: string): String {
+     static newReferenceString(referencePath: string): String {
         return '/// <reference path=\"' + referencePath + '\" />';
     }
 }
