244c0475678a4e7985be25851b75f1422e572eb1
Donald Jones <donald@getplatypi.com>
2014-12-08 16:34:59 -0600

    CLI-10 #time 1h expanded logic for templates with multiple interface files.

--- platypi-cli/handlers/references.handler.ts
+++ platypi-cli/handlers/references.handler.ts
@@ -11,28 +11,39 @@ var Promise = promises.Promise;
 class ReferencesHandler {
     /**
      *  Add a reference to the current project's _references.d.ts file.
-     *  @param referencePath The relative path to the interface to be referenced.
+     *  @param referenceFolderPath The relative path to the folder containing an interface to be referenced.
      *  @param referenceType The control type of the interface.
      */
-    static addReference(referenceFileLocation: string, referencePath: string, referenceType?: string): Thenable<any> {
-        if (referencePath.indexOf('.d.ts') > -1) {
-            return fileutils.readFile(referenceFileLocation).then((referenceData: string) => {
-                var typePos = -1;
+    static addReference(referenceFileLocation: string, referenceFolderPath: string, referenceType?: string): Thenable<any> {
+        return this.findInterfaceFiles(referenceFolderPath).then((interfaceFiles) => {
+            if (interfaceFiles.length > 0) {
+                return Promise.all(<Array<Promise<string>>>interfaceFiles.map((file) => {
+                    if (file.indexOf('.d.ts') > -1) {
+                        return fileutils.readFile(referenceFileLocation).then((referenceData: string) => {
+                            var typePos = -1;
 
-                if (referenceType) {
-                    var referenceTypeComment = '// ' + referenceType.toLowerCase();
+                            if (referenceType) {
+                                var referenceTypeComment = '// ' + referenceType.toLowerCase();
 
-                    typePos = referenceData.indexOf(referenceTypeComment);
-                    typePos = typePos + referenceTypeComment.length;
+                                typePos = referenceData.indexOf(referenceTypeComment);
+                                typePos = typePos + referenceTypeComment.length;
 
-                    return fileutils.appendFileAt(referenceFileLocation, typePos, '\n' + this.newReferenceString(referencePath));
-                } else {
-                    return fileutils.appendFIle(referenceFileLocation, '\n' + this.newReferenceString(referencePath));
-                }
-            });
-        } else {
-           return Promise.resolve('');
-        }
+                                return fileutils.appendFileAt(referenceFileLocation, typePos, '\n'
+                                    + this.newReferenceString(referenceFolderPath));
+                            } else {
+                                return fileutils.appendFIle(referenceFileLocation, '\n' + this.newReferenceString(referenceFolderPath));
+                            }
+                        });
+                    } else {
+                        return Promise.resolve('');
+                    }
+                })).then(() => {
+                    return '';
+                });
+            } else {
+                return Promise.resolve('');
+            }
+        });
     }
 
     /**
@@ -60,7 +71,6 @@ class ReferencesHandler {
              return interfaceFiles;
          });
      }
-     
 }
 
 export = ReferencesHandler;
