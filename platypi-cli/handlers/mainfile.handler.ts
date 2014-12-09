1951086646c9d8a614d3b92da6cd2fe045bfb7de
Donald Jones <donald@getplatypi.com>
2014-12-08 18:22:31 -0600

    PCI-11 #time 1h Added logic for dealing with typescript control files in the main.ts file.

--- platypi-cli/handlers/mainfile.handler.ts
+++ platypi-cli/handlers/mainfile.handler.ts
@@ -1,12 +1,77 @@
 /// <reference path="../_references.d.ts" />
-import fileutils = require('../utils/file.utils');
 
+import path = require('path');
+import fileutils = require('../utils/file.utils');
+import promises = require('es6-promise');
+
+var Promise = promises.Promise;
+
+/**
+ *  Contains methods for handling a project's main.ts file.
+ */
 class MainFileHandler {
-    static addControl(controlPath: string, projectConfig: config.IPlatypi) {
-        controlPath = (controlPath.charAt(0) === '.' ? controlPath : './' + controlPath);
-        var toAppend = 'require(\'' + controlPath + '\');\n';
-        return fileutils.appendFIle(projectConfig.mainFile, toAppend);
+    /**
+     *  Add a 'require' reference for a control to the current project's main.ts file.
+     *  @param controlFolderPath The absolute path to the folder containing the control.
+     *  @param projectCOnfig The absolute path to the project's config file.
+     */
+    static addControl(controlFolderPath: string, projectConfig: config.IPlatypi, controlType?: string) {
+        var relativePathToControl = path.relative(projectConfig.mainFile, controlFolderPath);
+
+        return this.findTypeScriptFiles(controlFolderPath).then((tsFiles) => {
+            if (tsFiles.length > 0) {
+                return Promise.all(<Array<Promise<string>>>tsFiles.map((file) => {
+                    return fileutils.readFile(projectConfig.mainFile, { encoding: 'utf8' }).then((mainData: string) => {
+                        var typePos = -1,
+                            relativePathToControlFile = path.join(relativePathToControl, file);
+
+                        relativePathToControlFile = relativePathToControlFile.slice(3);
+                        relativePathToControlFile = './' + relativePathToControlFile;
+
+                        if (controlType) {
+                            var controlTypeComment = '// ' + controlType.toLowerCase() + 's';
+
+                            typePos = mainData.indexOf(controlTypeComment);
+                            typePos = typePos + controlTypeComment.length;
+                        }
+
+                        return fileutils.appendFileAt(projectConfig.mainFile, typePos, '\n'
+                            + this.newRequireString(relativePathToControlFile));
+                    });
+                })).then(() => {
+                    return '';
+                });
+            } else {
+                return Promise.resolve('');
+            }
+        });
     }
+
+    /**
+     *  Creates a new 'require' string from the provided control path.
+     *  @param controlPath The relative path to the control to be required.
+     */
+    static newRequireString(controlPath: string): string {
+        return 'require(\'' + controlPath + '\');\n';
+    }
+
+    /**
+     *  Search a directory for TypeScript files.
+     *  @param templatePath The path to the template directory to reference.
+     */
+     static findTypeScriptFiles(templatePath: string): Thenable<Array<string>> {
+         return fileutils.readdir(templatePath).then((fileList) => {
+             var tsFiles: Array<string> = [];
+
+             fileList.forEach((file) => {
+                 if (file.indexOf('.ts') > -1 && file.indexOf('.d.ts') === -1) {
+                     tsFiles.push(file);
+                 }
+             });
+
+             return tsFiles;
+         });
+     }
 }
 
 export = MainFileHandler;
