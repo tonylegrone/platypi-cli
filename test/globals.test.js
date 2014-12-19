4b4ded28391c436061937ae7344c71786484341a
Donald Jones <donald@getplatypi.com>
2014-12-19 16:46:06 -0600

    Added test for globals.js

---
+++ test/globals.test.js
@@ -0,0 +1,14 @@
+
+var chai = require('chai')
+    , sinon = require('sinon')
+    , sinonChai = require('sinon-chai')
+    , expect = chai.expect
+    , globals = require('../platypi-cli/globals');
+
+chai.use(sinonChai);
+
+describe('globals', function() {
+    describe('identifyApplication()', function() {
+        
+    });
+});
