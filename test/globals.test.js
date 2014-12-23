f7362e2bafc7d9c76bea2aac8c2b5f115a1a6d3f
Donald Jones <donald@getplatypi.com>
2014-12-23 10:51:21 -0600

    Finished implementing globals test.

--- test/globals.test.js
+++ test/globals.test.js
@@ -8,7 +8,33 @@ var chai = require('chai')
 chai.use(sinonChai);
 
 describe('globals', function() {
+    var sandbox, msgLabel, msgLog; 
+
+    before(function(done) {
+       sandbox = sinon.sandbox.create();
+       
+       msgLog = sandbox.stub(globals.console, 'log', function(message) {
+           return message;
+       });
+
+       msgLabel = sandbox.stub(globals.console, 'label', function(label) {
+           return label;
+       });
+
+       done();
+    });
+
+    after(function(done) {
+        sandbox.restore();
+        done();
+    });
+
     describe('identifyApplication()', function() {
-        
+        it('should output using msg.label and msg.log', function(done) {
+            globals.identifyApplication();
+            expect(msgLabel).to.have.been.calledWith('Platypi Command Line Interface');
+            expect(msgLog).to.have.been.calledWith('Version ' + globals.package.version);
+            done();
+        });
     });
 });
