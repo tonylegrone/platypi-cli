421ce814a61b588e9bcaa171c3b7a873ddd428e4
Donald Jones <donald@getplatypi.com>
2014-12-29 16:46:40 -0600

    Replaced template helper tests with more useful test cases.

--- test/helpers/template.helper.test.js
+++ test/helpers/template.helper.test.js
@@ -1,132 +1,250 @@
-var should = require('should') // jshint ignore:line
-    , path = require('path')
+var chai = require('chai')
     , fs = require('fs')
-    , util = require('util')
-    , package = require('../../package.json')
+    , sinon = require('sinon')
+    , sinonChai = require('sinon-chai')
+    , expect = chai.expect
+    , Promise = require('es6-promise').Promise
     , TemplateHelper = require('../../platypi-cli/helpers/template.helper')
-    , GithubService = require('../../platypi-cli/services/github/github.service')
-    , directory = require('../../platypi-cli/utils/directory.utils');
+    , ZipUtil = require('../../platypi-cli/utils/zip/zip.util')
+    , package = require('../../package.json');
 
-describe('template helper', function () {
-    var helper = new TemplateHelper(GithubService);
+chai.use(sinonChai); 
 
-    it('should be an object', function () {
-        helper.should.be.an.Object;
-    });
+// Dummy function to return a fake service obj
+var MockService = (function() {
+    function MockService() {
+    }
 
-    describe('package.json', function () {
-        it('should be an object', function () {
-            should.exist(package);
-            package.should.be.an.Object;
+    MockService.prototype.getRelease = function(version, filepath) {
+        return Promise.resolve(filepath);
+    };
+
+    return MockService;
+})();
+
+// Tests
+
+describe('Template Helper', function() {
+    var helper = new TemplateHelper(MockService);
+
+    describe('__makeCacheDir method', function() {
+        var sandbox, mkdirFunc; 
+        beforeEach(function(done) {
+            sandbox = sinon.sandbox.create();
+
+            mkdirFunc = sandbox.stub(fs, 'mkdir', function(folder, mode, callback) {
+                if (folder === 'folder/doesnt/exist/cache') {
+                    callback(null);
+                } else if (folder === 'folder/already/exists/cache') {
+                    callback({ code : 'EEXIST' });
+                } else {
+                    callback({ code : 'OTHERERROR' });
+                }
+            });
+            
+            done();
         });
 
-        it('should contain a version number', function () {
-            should.exist(package.version);
+        afterEach(function(done) {
+            sandbox.restore();
+            done();
         });
-    });
 
-    describe('__makeCache method', function () {
-        var cachePath = '';
-        before(function (done) {
-            directory.appDataDir()
-                .then(function (appDataPath) {
-                    return helper.__makeCacheDir(appDataPath);
-                })
-                .then(function (cacheDir) {
-                    cachePath = cacheDir;
+        it('should return an error since something went wrong', function(done) {
+            helper.__makeCacheDir('cause/an/error').then(function(cacheDir) {
+                try {
+                    expect(cacheDir).to.not.exist;
                     done();
-                }, function (err) {
-                    throw err;
-                });
+                } catch (e) {
+                    done(e);
+                }
+            }, function(err) {
+                try {
+                    expect(err).to.be.an.object;
+                    expect(err.code).to.be.an.object;
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
         });
 
-        it('should make the cache dir', function () {
-            cachePath.should.not.equal('');
+        it('should return the cachefolder that exists', function(done) {
+            helper.__makeCacheDir('folder/already/exists').then(function(cacheFolder) {
+                try {
+                    expect(cacheFolder).to.equal('folder/already/exists/cache');
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
         });
 
+        it('should create a cacheFolder', function(done) {
+            helper.__makeCacheDir('folder/doesnt/exist').then(function(cacheFolder) {
+                try {
+                    expect(cacheFolder).to.equal('folder/doesnt/exist/cache');
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
+        });
     });
 
-    describe('__makeArchiveCacheDir method', function () {
-        var archiveCachePath = '';
-        before(function (done) {
-            directory.appDataDir()
-                .then(function (appDataPath) {
-                    return helper.__makeCacheDir(appDataPath);
-                })
-                .then(function (cacheDir) {
-                    return helper.__makeArchiveCacheDir(cacheDir);
-                }, function (err) {
-                    throw err;
-                })
-                .then(function (archiveDir) {
-                    archiveCachePath = archiveDir;
+    describe('__makeArchiveCacheDir method', function() {
+        var sandbox, mkdirFunc; 
+        beforeEach(function(done) {
+            sandbox = sinon.sandbox.create();
+
+            mkdirFunc = sandbox.stub(fs, 'mkdir', function(folder, mode, callback) {
+                if (folder === 'folder/doesnt/exist/archives/') {
+                    callback(null);
+                } else if (folder === 'folder/already/exists/archives/') {
+                    callback({ code : 'EEXIST' });
+                } else {
+                    callback({ code : 'OTHERERROR' });
+                }
+            });
+            
+            done();
+        });
+
+        afterEach(function(done) {
+            sandbox.restore();
+            done();
+        });
+
+        it('should return an error since something went wrong', function(done) {
+            helper.__makeArchiveCacheDir('cause/an/error').then(function(archiveDir) {
+                try {
+                    expect(archiveDir).to.not.exist;
                     done();
-                }, function (err) {
-                    throw err;
-                });
+                } catch (e) {
+                    done(e);
+                }
+            }, function(err) {
+                try {
+                    expect(err).to.be.an.object;
+                    expect(err.code).to.be.an.object;
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
         });
 
-        it('should make the cache dir', function () {
-            archiveCachePath.should.not.equal('');
+        it('should return the archive folder that exists', function(done) {
+            helper.__makeArchiveCacheDir('folder/already/exists').then(function(archiveDir) {
+                try {
+                    expect(archiveDir).to.equal('folder/already/exists/archives/');
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
         });
 
+        it('should create a archive folder', function(done) {
+            helper.__makeArchiveCacheDir('folder/doesnt/exist').then(function(archiveDir) {
+                try {
+                    expect(archiveDir).to.equal('folder/doesnt/exist/archives/');
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            });
+        });
     });
 
-    describe('__downloadTemplates', function () {
-        var templatePath = '';
+    describe('updateTemplates function', function() {
+        var sandbox, mkdirFunc, getReleaseMethod, extractAllMethod, version;
 
-        before(function (done) {
-            directory.appDataDir()
-                .then(function (appDir) {
-                    return helper.__downloadTemplates(appDir);
-                })
-                .then(function (tmpPath) {
-                    templatePath = tmpPath;
+        version = true;
+
+        beforeEach(function(done) {
+            // dummy functions
+            var cliConfig = { setConfig: function() { } };
+
+            sandbox = sinon.sandbox.create();
+
+            mkdirFunc = sandbox.stub(fs, 'mkdir', function(dirpath, mode, callback) {
+                callback(null);
+            });
+
+            sandbox.stub(package, 'version', function() {
+                if (version) {
+                    return '0.0.1';
+                } else {
+                    return undefined;
+                }
+            }());
+
+            sandbox.stub(fs, 'readFile', function(filepath, options, callback) {
+                var testConfig = {
+                    templates: {
+                        lastUpedate: new Date(),
+                        baseLocation: ''
+                    }
+                };
+
+                callback(null, JSON.stringify(testConfig));
+            });
+
+            sandbox.stub(ZipUtil.prototype, 'extractAll', function(extractDir, overwrite) {
+                if (extractDir === 'test/dir/fail/cache/0.0.1') {
+                    throw 'Error';
+                }
+                return extractDir;
+            });
+
+
+            sandbox.stub(cliConfig, 'setConfig', function(cfg) {
+                return Promise.resolve(cfg);
+            });
+
+            done();
+        });
+
+        afterEach(function(done) {
+            sandbox.restore();
+            version = (version ? false : true);
+            done();
+        });
+
+        it('should download, unzip, and update the cli config', function(done) {
+            helper.updateTemplates('test/dir').then(function(extractDir) {
+                try {
+                    expect(extractDir).to.equal('test/dir/cache/0.0.1/platypi-cli-templates-0.0.1');
                     done();
-                }, function (err) {
-                    throw err;
-                });
+                } catch (e) {
+                    done(e);
+                }
+            }, function(err) {
+                done(err);
+            });
         });
 
-        it('should download a template archive', function () {
-            templatePath.should.not.equal('');
-        });
-        
-    });
-
-    describe('updateTemplates', function () {
-        var extractedTemplatePath = ''
-            , appDataDir = '';
-
-        before(function (done) {
-            directory.appDataDir()
-                .then(function (appDir) {
-                    appDataDir = appDir;
-                    return helper.updateTemplates(appDir);
-                })
-                .then(function (tmpPath) {
-                    extractedTemplatePath = tmpPath;
+        it('should download, unzip, and update the cli config defaulting to a version number', function(done) {
+            helper.updateTemplates('test/dir').then(function(extractDir) {
+                try {
+                    expect(extractDir).to.equal('test/dir/cache/0.0.1/platypi-cli-templates-0.0.1');
                     done();
-                }, function (err) {
-                    throw err;
-                });
+                } catch (e) {
+                    done(e);
+                }
+            }, function(err) {
+                done(err);
+            });
         });
 
-        it('should download and extract the templates', function () {
-            extractedTemplatePath.should.not.equal('');
-        });
 
-        it('should update the cli config file', function () {
-            var cliConfigFile = path.normalize(util.format('%s/%s', appDataDir, 'cli.json'));
-
-            fs.readFile(cliConfigFile, 'utf8', function (err, data) {
-                should.not.exist(err);
-
-                var config = JSON.parse(data);
-
-                should.exist(config);
-
-                config.version.should.exist;
+        it('should fail because there was an error unzipping', function(done) {
+            helper.updateTemplates('test/dir/fail').then(function(extractDir) {
+                done('should not return from here');
+            }, function(err) {
+                expect(err).to.equal('Error');
+                done();
             });
         });
 
