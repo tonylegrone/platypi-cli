1db35ddab0c151645086b411466c96c6d6ce4cba
Donald Jones <donald@getplatypi.com>
2015-01-07 10:09:28 -0600

    CLI-21 #time 30m Changed view to match new interaction.

--- platypi-cli/views/project/new/new.project.view.ts
+++ platypi-cli/views/project/new/new.project.view.ts
@@ -1,11 +1,18 @@
 import BaseView = require('../../base.view');
 
 class NewProjectView extends BaseView {
-    constructor() {
-        super('');
-    }
-    display() {
-        this.logger.log('New Project created at: ' + this.responseText);
+    setResponse(response: string, error?: string): any {
+        console.log('here');
+        if (error) {
+            if (this._debug) {
+                super.setResponse('', 'Project Create Controller: ' + error);
+                return;
+            }
+            super.setResponse('', 'An error occurrend and your project could not be created!');
+            return;
+        }
+
+        super.setResponse('New Project Created at: ' + response);
     }
 }
 
