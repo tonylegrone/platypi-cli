15cd8038cc3b0b5cb8236b40a991435215a0b3a0
Donald Jones <donald@getplatypi.com>
2015-01-07 10:03:14 -0600

    CLI-21 #time 30m Changed the interaction between the controllers and views.

--- platypi-cli/views/base.view.ts
+++ platypi-cli/views/base.view.ts
@@ -1,13 +1,31 @@
 /// <reference path="../_references.d.ts" />
 import msg = require('../helpers/msg.helper');
+import globals = require('../globals');
 
 class BaseView implements IView {
-    logger = msg;
-    constructor(public responseText?: string) {}
+    public logger = msg;
+    private __responseText = '';
+    private __errorText = '';
+    _debug = globals.debug || false;
+
+    constructor() {}
+
+    setResponse(response: string, error?: string): any {
+       if (error) {
+           this.__errorText = error;
+           return;
+       }
+
+       this.__responseText = response;
+    }
 
     display() {
-        this.logger.log(this.responseText);
-        return;
+        if (this.__errorText !== '') {
+            this.logger.error(this.__errorText);
+            return;
+        }
+
+        this.logger.log(this.__responseText);
     }
 }
 
