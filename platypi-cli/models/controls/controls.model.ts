3df38cc82a1e7220a5d9c76bd2c8e8b9758c09b5
pwdonald <donald@getplatypi.com>
2015-01-23 11:19:15 -0600

    CLI-22 #time 1h Updated controls model for list command.

--- platypi-cli/models/controls/controls.model.ts
+++ platypi-cli/models/controls/controls.model.ts
@@ -14,16 +14,73 @@ class ControlsModel implements IModel {
     public extendsClass: string;
     public errorMessage: string;
     public successMessage: string;
+    public controlsTable: Array<string>;
 
-    constructor(type: string, name: string, registeredName?: string, extendsClass?: string) {
+    constructor(type: string, name?: string, registeredName?: string, extendsClass?: string) {
         this.type = type.toLowerCase().trim();
-        this.name = name.trim();
+        this.name = (name && name !== '' ? name.trim() : 'new' + type);
         this.registeredName = (registeredName && registeredName !== '' ? registeredName.trim() : this.name);
         this.extendsClass = (extendsClass && extendsClass !== '' ? extendsClass.trim() : '');
     }
 
     private __getConfig(): Thenable<config.IPlatypi> {
         return ConfigFinder.findConfig().then(PlatypiConfig.loadFromObject);
+    }
+
+    list(): Thenable<Array<string>> {
+        var controlsTable: Array<string> = [];
+
+        return this.__getConfig().then((config) => {
+            // attributecontrols
+            controlsTable.push('Attribute Controls');
+            config.attributecontrols.forEach((attributecontrol) => {
+                controlsTable.push(this.__formatControlString(attributecontrol));
+            });
+
+            // injectables
+            controlsTable.push('Injectable Controls');
+            config.injectables.forEach((injectable) => {
+                controlsTable.push(this.__formatControlString(injectable));
+            });
+
+            // models
+            controlsTable.push('Models');
+            config.models.forEach((model) => {
+                controlsTable.push(this.__formatControlString(model));
+            });
+
+            // repositories
+            controlsTable.push('Repositories');
+            config.repositories.forEach((repository) => {
+                controlsTable.push(this.__formatControlString(repository));
+            });
+
+            // services
+            controlsTable.push('Services');
+            config.services.forEach((service) => {
+                controlsTable.push(this.__formatControlString(service));
+            });
+
+            // templatecontrols
+            controlsTable.push('Template Controls');
+            config.templatecontrols.forEach((template) => {
+                controlsTable.push(this.__formatControlString(template));
+            });
+
+            // viewcontrols
+            controlsTable.push('View Controls');
+            config.viewcontrols.forEach((viewcontrol) => {
+                controlsTable.push(this.__formatControlString(viewcontrol));
+            });
+
+            this.controlsTable = controlsTable;
+
+            return controlsTable;
+        });
+    }
+
+    private __formatControlString(control: config.IPlatypusControl): string {
+        return control.name + ' path: ' + control.path;
     }
 
     create(): Thenable<string> {
