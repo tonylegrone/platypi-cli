a3305279a1511fa3debe8edf9625e25f5ad305f3
pwdonald <donald@getplatypi.com>
2015-01-23 11:19:36 -0600

    CLI-22 #time 1h Added controller for list command.

---
+++ platypi-cli/controllers/controls/list/list.controls.controller.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="../../../_references.d.ts" />
+import Model = require('../../../models/controls/controls.model');
+
+class ListControlsController implements IController {
+    public model;
+
+    constructor(public view: IView, type: string) {
+        this.model = new Model(type);
+        this.view.model = this.model;
+    }
+
+    list(): Thenable<string> {
+        return this.model.list();
+    }
+
+    getResponseView(): Thenable<IView> {
+        return this.list().then((controlsTable) => {
+            this.model.successMessage = 'Controls Table';
+            return this.view;
+        }, (err) => {
+            this.model.errorMessage = err;
+            return this.view;
+        });
+    }
+}
+
+export = ListControlsController;
